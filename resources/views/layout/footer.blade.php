ef4fe4f67fe1ceabfc2ebc84722bec3efa8e744a
chidokato <tuan.pn92@gmail.com>
2024-05-04 09:23:59 +0700

    sdf

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -5,36 +5,46 @@
                 <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
                     <div class="row">
                         <div class="col-lg-6">
-                            <h4 class="title"><span>CTCP GLS EDUCATION</span></h4>
-                            <img src="data/home/logogls.png" alt="No picture">
-                            <p>Địa chỉ:  16 TT4A, Khu đô thị Văn Quán, Hà Đông, Hà Nội</p>
-                            <p>Điện thoại: 0973224725</p>
-                            <p>Mail:  info@duhocgls.edu.vn</p>
-                            <p>Mã số thuế: 0110346219</p>
-                            <p>Ngày cấp: 28/11/2023</p>
-                            <p>Nơi cấp: Cục thuế Thành phố Hà Nội</p>
+                            <h4 style="margin-top: 24px;" class="title">CTCP GLS EDUCATION</h4>
+                            <img class="logo" src="data/home/logogls.png" alt="No picture">
+                            <p><strong>Địa chỉ:</strong>  16 TT4A, Khu đô thị Văn Quán, Hà Đông, Hà Nội</p>
+                            <p><strong>Điện thoại:</strong> 0973224725</p>
+                            <p><strong>Mail:</strong>  info@duhocgls.edu.vn</p>
+                            <p><strong>Mã số thuế:</strong> 0110346219</p>
+                            <p><strong>Ngày cấp:</strong> 28/11/2023</p>
+                            <p><strong>Nơi cấp:</strong> Cục thuế Thành phố Hà Nội</p>
                         </div>
                         <div class="col-lg-6">
-                            <h4 class="title"><span>GSG CTCP TẬP ĐOÀN ĐẦU TƯ VÀ PHÁT TRIỂN GIẢI PHÁP XANH</span></h4>
-                            <img src="data/home/logo1.png" alt="No picture">
-                            <p>Địa chỉ: Tầng 5, số nhà 85 Đường Nguyễn Chí Thanh, Phường Láng Hạ, Quận Đống Đa, Hà Nội</p>
-                            <p>Điện thoại: 0983366158</p>
-                            <p>Mail: giaiphapxanh.vn04@gmail.com</p>
-                            <p>Mã số thuế: 0110459043</p>
-                            <p>Ngày cấp: 20/12/2023</p>
-                            <p>Nơi Cấp: Cục thuế Thành phố Hà Nội</p>
+                            <h4 class="title">GSG CTCP TẬP ĐOÀN ĐẦU TƯ VÀ PHÁT TRIỂN GIẢI PHÁP XANH</h4>
+                            <img class="logo" src="data/home/logo1.png" alt="No picture">
+                            <p><strong>Địa chỉ:</strong> Tầng 5, số nhà 85 Đường Nguyễn Chí Thanh, Phường Láng Hạ, Quận Đống Đa, Hà Nội</p>
+                            <p><strong>Điện thoại:</strong> 0983366158</p>
+                            <p><strong>Mail:</strong> giaiphapxanh.vn04@gmail.com</p>
+                            <p><strong>Mã số thuế:</strong> 0110459043</p>
+                            <p><strong>Ngày cấp:</strong> 20/12/2023</p>
+                            <p><strong>Nơi Cấp:</strong> Cục thuế Thành phố Hà Nội</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
-                    <h3 style="color:#ffffff">ĐĂNG KÝ NHẬN THÔNG TIN</h3>
-                    <div class="input_email">
-                        <input type="text" placeholder="Email">
-                        <i class="far fa-envelope"></i>
+                    <h4 style="margin-top: 24px;" class="title">LIÊN HỆ</h4>
+                    <p>Nếu bạn QUAN TÂM, hãy để lại THÔNG TIN để nhận tư vấn MIỄN PHÍ từ đội ngũ cố vấn chuyên môn của GLS</p>
+                    
+                    <div class="form">
+                        <form>
+                            <div class="input_email">
+                                <input type="text" placeholder="Họ và Tên">
+                            </div>
+                            <div class="input_email">
+                                <input type="text" placeholder="Số điện thoại">
+                            </div>
+                            <div class="input_email">
+                                <input type="text" placeholder="Email">
+                            </div>
+                            <button class="button_signUp" type="submit">Gửi đi</button>
+                        </form>
                     </div>
-                    <a href="#">
-                        <button class="button_signUp">Đăng ký ngay</button>
-                    </a>
+
                 </div>
             </div>
         </div>
